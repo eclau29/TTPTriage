1e4074c44307adff2194e6ba1f9e2c7f9dfe2cbf
Xian Zheng <xzheng2112@gmail.com>
2019-10-25 17:42:02 -0600

    adding angular components

--- TriageAngular/src/app/app.module.ts
+++ TriageAngular/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { VitalsComponent } from './vitals/vitals.component';
+import { SymptomsComponent } from './symptoms/symptoms.component';
+import { GeneralInfoComponent } from './general-info/general-info.component';
+import { ScannerComponent } from './scanner/scanner.component';
+import { SeverityComponent } from './severity/severity.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    VitalsComponent,
+    SymptomsComponent,
+    GeneralInfoComponent,
+    ScannerComponent,
+    SeverityComponent
   ],
   imports: [
     BrowserModule,
---
+++ TriageAngular/src/app/severity/severity.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-severity',
+  templateUrl: './severity.component.html',
+  styleUrls: ['./severity.component.css']
+})
+export class SeverityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
