1edb86954f2c8562efdf6ea4f3636c47e47fc971
Emily Lau <emilylau@Brucebs-MacBook-Pro-2.local>
2019-10-25 17:44:22 -0600

    added a boot project

---
+++ TTPTriage/src/main/java/com/ttptriage/TtpTriageApplication.java
@@ -0,0 +1,13 @@
+package com.ttptriage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtpTriageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtpTriageApplication.class, args);
+	}
+
+}
