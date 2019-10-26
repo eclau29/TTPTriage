ad721eb59af0433ed7a446960545b15a3dbe0e6d
Samantha Janik <midgetmode19@gmail.com>
2019-10-25 22:53:06 -0600

    Added catastrophe to db

--- DB/ttptriagedb.sql
+++ DB/ttptriagedb.sql
@@ -16,17 +16,37 @@ CREATE SCHEMA IF NOT EXISTS `ttptriage` DEFAULT CHARACTER SET utf8 ;
 USE `ttptriage` ;
 
 -- -----------------------------------------------------
+-- Table `catastrophe`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `catastrophe` ;
+
+CREATE TABLE IF NOT EXISTS `catastrophe` (
+  `cat_id` INT(11) NOT NULL,
+  `cat_name` VARCHAR(45) NULL,
+  `cat_location` VARCHAR(45) NULL,
+  PRIMARY KEY (`cat_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `person`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `person` ;
 
 CREATE TABLE IF NOT EXISTS `person` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `location` VARCHAR(25) NULL DEFAULT 'Ground Zero',
-  `gps_location` VARCHAR(100) NULL,
+  `id` INT NOT NULL,
+  `location` VARCHAR(25) NOT NULL DEFAULT 'Ground Zero',
+  `gps_location` VARCHAR(100) NOT NULL,
   `initial_eval_time` VARCHAR(45) NULL,
   `gender` VARCHAR(12) NULL,
-  PRIMARY KEY (`id`))
+  `catastrophe_cat_id` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_person_catastrophe1_idx` (`catastrophe_cat_id` ASC),
+  CONSTRAINT `fk_person_catastrophe1`
+    FOREIGN KEY (`catastrophe_cat_id`)
+    REFERENCES `catastrophe` (`cat_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -36,7 +56,7 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `vitals` ;
 
 CREATE TABLE IF NOT EXISTS `vitals` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL,
   `diastolic_bp` INT NULL,
   `systolic_bp` INT NULL,
   `pulse` INT NULL,
@@ -60,7 +80,7 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `personal_info` ;
 
 CREATE TABLE IF NOT EXISTS `personal_info` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL,
   `first_name` VARCHAR(45) NULL,
   `last_name` VARCHAR(45) NULL,
   `middle_name` VARCHAR(45) NULL,
@@ -82,9 +102,9 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `symptoms` ;
 
 CREATE TABLE IF NOT EXISTS `symptoms` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `body_part` VARCHAR(45) NULL,
-  `injury` VARCHAR(45) NULL,
+  `id` INT NOT NULL,
+  `body_part` VARCHAR(45) NOT NULL,
+  `injury` VARCHAR(45) NOT NULL,
   `person_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_symptoms_person1_idx` (`person_id` ASC),
@@ -106,43 +126,3 @@ GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'ttptri'@'localhost'
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
--- -----------------------------------------------------
--- Data for table `person`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `ttptriage`;
-INSERT INTO `person` (`id`, `location`, `gps_location`, `initial_eval_time`, `gender`) VALUES (1, 'Denver', NULL, NULL, 'f');
-
-COMMIT;
-
-
--- -----------------------------------------------------
--- Data for table `vitals`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `ttptriage`;
-INSERT INTO `vitals` (`id`, `diastolic_bp`, `systolic_bp`, `pulse`, `resp_rate`, `pulse_ox`, `severity`, `person_id`) VALUES (1, 110, 60, 60, 30, 98, 'GREEN', 1);
-
-COMMIT;
-
-
--- -----------------------------------------------------
--- Data for table `personal_info`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `ttptriage`;
-INSERT INTO `personal_info` (`id`, `first_name`, `last_name`, `middle_name`, `dob`, `person_id`) VALUES (DEFAULT, 'Jane', 'Doe', NULL, '1980-12-12', 1);
-
-COMMIT;
-
-
--- -----------------------------------------------------
--- Data for table `symptoms`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `ttptriage`;
-INSERT INTO `symptoms` (`id`, `body_part`, `injury`, `person_id`) VALUES (1, 'leg', 'laceration', 1);
-
-COMMIT;
-
