362f18e60060be11d31d12b3ac7b689a5b268073
Xian Zheng <xzheng2112@gmail.com>
2019-10-25 22:14:31 -0600

    Symptoms header

--- TriageAngular/src/app/app-routing.module.ts
+++ TriageAngular/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { SymptomsComponent } from './symptoms/symptoms.component';
 import { SeverityComponent } from './severity/severity.component';
 import { VitalsComponent } from './vitals/vitals.component';
 import { NgModule } from '@angular/core';
@@ -6,7 +7,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: 'vitals', component: VitalsComponent},
-  { path: 'severity', component: SeverityComponent}
+  { path: 'severity', component: SeverityComponent},
+  { path: 'symptoms', component: SymptomsComponent}
 ];
 
 @NgModule({
