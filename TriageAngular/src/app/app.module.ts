87a7ad22ea7defd218ed878cc90e1b8cbb105a87
Xian Zheng <xzheng2112@gmail.com>
2019-11-14 18:42:58 -0700

    tracking lat long on severity page

--- TriageAngular/src/app/app.module.ts
+++ TriageAngular/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { QRCodeModule } from 'angularx-qrcode';
+import { GoogleMapsModule } from '@angular/google-maps';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VitalsComponent } from './vitals/vitals.component';
@@ -64,6 +65,7 @@ import { MenuComponent } from './menu/menu.component';
   imports: [
     BrowserModule,
     QRCodeModule,
+    GoogleMapsModule,
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
