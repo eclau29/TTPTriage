074a2b94240fd53020a2297b6c83ceefdaf8212e
Samantha Janik <midgetmode19@gmail.com>
2019-11-15 22:44:50 -0700

    added constructors to models and moved lat/long to scanner

--- TriageAngular/src/app/severity/severity.component.ts
+++ TriageAngular/src/app/severity/severity.component.ts
@@ -1,7 +1,5 @@
-import { Person } from './../../models/person';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-severity',
@@ -9,25 +7,9 @@ import { Observable } from 'rxjs';
   styleUrls: ['./severity.component.css']
 })
 export class SeverityComponent implements OnInit {
-  // Class Fields
-  person: Person = new Person();
-  center: google.maps.LatLngLiteral;
-
-  state$: Observable<string>;
 
   constructor(public activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    this.person.barcodeNum = window.history.state.barcodeNum;
-    console.log(this.person);
-  }
-  getLatLong() {
-    navigator.geolocation.getCurrentPosition(position => {
-      this.center = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
-      };
-      console.log(this.center);
-    });
   }
 }
--- TriageAngular/src/models/vitals.ts
+++ TriageAngular/src/models/vitals.ts
@@ -1,3 +1,5 @@
+import { Person } from './person';
+
 export class Vitals {
   id: number;
   diastolicBloodPressure: number;
@@ -5,6 +7,26 @@ export class Vitals {
   respirationRate: number;
   pulseOx: number;
   timestamp: Date;
-//   severity: Severity ;
+  severity: Severity;
+  person: Person;
 
+  constructor(
+    id?: number,
+    diastolicBloodPressure?: number,
+    systolicBloodPressure?: number,
+    respirationRate?: number,
+    pulseOx?: number,
+    timestamp?: Date,
+    severity?: Severity,
+    person?: Person
+  ) {
+    this.id = id;
+    this.diastolicBloodPressure = diastolicBloodPressure;
+    this.systolicBloodPressure = systolicBloodPressure;
+    this.respirationRate = respirationRate;
+    this.pulseOx = pulseOx;
+    this.timestamp = timestamp;
+    this.severity = severity;
+    this.person = person;
+  }
 }
